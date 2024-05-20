cc5999b1d142a725cb9d6c740b87b57e87047478
Gustavo <Gustavofarias@gmail.com>
2024-05-20 18:56:37 -0300

    atualização commit

--- main.js
+++ main.js
@@ -8,17 +8,18 @@ form.addEventListener('submit', function(e){
     const campoValorA = Number(campoA.value)
     const campoValorB = Number(campoB.value)
 
-    const menssagemError = `[ERROR] O número do campo <b>A</b> precisa ser maior! Tente novamente.`
-    const menssagemSucesso = `Parabéns o campo <b>A</b> ${campoValorA} está maior que o campo <b>B</b> ${campoValorB}!`
+    const menssagemError = `[ERROR] O número do campo <b>B</b> precisa ser maior! Tente novamente.`
+    const menssagemSucesso = `Parabéns o campo <b>B</b> ${campoValorB} está maior que o campo <b>A</b> ${campoValorA}!`
 
-    if (campoValorA <= campoValorB) {
+    if (campoValorA >= campoValorB) {
         const campoFalse = document.querySelector('.error-menssagem')
-        campoA.classList.add('error')
+        campoB.classList.add('error')
         campoFalse.innerHTML = menssagemError;
         campoFalse.style.display = 'block'
         document.querySelector('.sucesso').style.display = 'none';
-    } else {
-        campoA.classList.remove('error')
+
+    } else {        
+        campoB.classList.remove('error')
         const campoVerdadeiro = document.querySelector('.sucesso');      
         campoVerdadeiro.innerHTML = menssagemSucesso;
         campoVerdadeiro.style.display = 'block';
